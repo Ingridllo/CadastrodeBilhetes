c7ec5e544839c3a0a977af70eebe2dc516c1a892
Ingrid Louise <98850812+Ingridllo@users.noreply.github.com>
2023-01-16 14:06:38 -0300

    Create Ticket.py

---
+++ Ticket.py
@@ -0,0 +1,38 @@
+class Ticket:
+   def _init_(self,name,date):
+       self.name = name
+       self.date = date
+       
+class TicketSystem:
+   def _init_(self):
+       self.tickets[]
+   
+   def add_ticket(self,name,date):
+   self.tickets.append(ticket)
+    print("Bilhete adicionado com sucesso!")
+    
+   def remove_ticket(self,name):
+     for i, ticket in enumerate(self.tickets):
+          if ticket.name == name:
+                      del
+                      self.tickets[1]
+                      print("Bilhete removido com sucesso!")
+                      return
+                   print("Bilhete não encontrado")
+   def list_tickets(self):
+    for i,ticket in
+    enumerate(self.tickets):
+              print(f"{i+1} {ticket.name} - {ticket.date})
+            if _name_ == "_main_":
+            system = TicketSystem()
+            system.add_ticket("Conferencia",24-04-2023)
+         syste.add_ticket("Show",25-06-2023")  
+         
+            system.list_tickets()
+            
+          sistem.remove_ticket("Show")
+            
+              
+              
+                   
+                     
